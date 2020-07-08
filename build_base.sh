ffc8e36401351fdf3dc92ae438a92a10487911d5
Leslie Yu <Leslie_Yu@asus.com>
2020-07-08 08:33:47 +0800

    Add support for building Tinker Board
    Change-Id: I7fa91f240ad7dd094b8cce3c5387723f0093600d

--- build_base.sh
+++ build_base.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [[ -z $ARCH || -z $LUNCH || -z $UBOOT_DEFCONFIG || -z $KERNEL_DEFCONFIG || -z $KERNEL_DTS ]];then
+if [[ -z $ARCH || -z $LUNCH || -z $UBOOT_DEFCONFIG || -z $KERNEL_DEFCONFIG || -z $KERNEL_DTS || -z $RELEASE_NAME ]];then
     echo "Missing some mandatory args, exit!"
     echo "ARCH=$ARCH"
     echo "LUNCH=$LUNCH"
     echo "UBOOT_DEFCONFIG=$UBOOT_DEFCONFIG"
     echo "KERNEL_DEFCONFIG=$KERNEL_DEFCONFIG"
     echo "KERNEL_DTS=$KERNEL_DTS"
+    echo "RELEASE_NAME=$RELEASE_NAME"
     exit 1
 fi
 
@@ -29,10 +30,11 @@ WIDEVINE_LEVEL=`get_build_var BOARD_WIDEVINE_OEMCRYPTO_LEVEL`
 
 #lunch $DEVICE-$BUILD_VARIANT
 
-PLATFORM_VERSION=`get_build_var PLATFORM_VERSION`
-DATE=$(date  +%Y%m%d.%H%M)
-STUB_PATH=Image/"$KERNEL_DTS"_"$PLATFORM_VERSION"_"$DATE"_RELEASE_TEST
-STUB_PATH="$(echo $STUB_PATH | tr '[:lower:]' '[:upper:]')"
+#PLATFORM_VERSION=`get_build_var PLATFORM_VERSION`
+#DATE=$(date  +%Y%m%d.%H%M)
+#STUB_PATH=Image/"$KERNEL_DTS"_"$PLATFORM_VERSION"_"$DATE"_RELEASE_TEST
+STUB_PATH=IMAGE/"$RELEASE_NAME"
+#STUB_PATH="$(echo $STUB_PATH | tr '[:lower:]' '[:upper:]')"
 export STUB_PATH=$PROJECT_TOP/$STUB_PATH
 export STUB_PATCH_PATH=$STUB_PATH/PATCHES
 
@@ -47,7 +49,7 @@ else
     exit 1
 fi
 
-cd u-boot && make ARCHV=$ARCHV distclean && make ARCHV=$ARCHV $UBOOT_DEFCONFIG && make ARCHV=$ARCHV -j$JOBS && cd -
+#cd u-boot && make ARCHV=$ARCHV distclean && make ARCHV=$ARCHV $UBOOT_DEFCONFIG && make ARCHV=$ARCHV -j$JOBS && cd -
 if [ $? -eq 0 ]; then
     echo "Build uboot ok!"
 else
@@ -64,7 +66,7 @@ fi
 
 # build kernel
 echo "Start build kernel"
-cd kernel && make ARCH=$ARCH distclean && make ARCH=$ARCH $KERNEL_DEFCONFIG && make ARCH=$ARCH $KERNEL_DTS.img -j$JOBS && cd -
+#cd kernel && make ARCH=$ARCH distclean && make ARCH=$ARCH $KERNEL_DEFCONFIG && make ARCH=$ARCH $KERNEL_DTS.img -j$JOBS && cd -
 if [ $? -eq 0 ]; then
     echo "Build kernel ok!"
 else
@@ -72,13 +74,21 @@ else
     exit 1
 fi
 # build wifi ko
-source device/rockchip/common/build_wifi_ko.sh
+#source device/rockchip/common/build_wifi_ko.sh
 
+ASUS_CSC_BUILD_NUMBER=WW_"$BUILD_NUMBER"
+ASUS_PROJECT_VERSION=$BUILD_NUMBER
 
 # build android
 echo "start build android"
 make installclean
-make -j$JOBS
+if [ "$BUILD_OTA" = true ] ; then
+	echo "generate ota package"
+	make BUILD_NUMBER=$BUILD_NUMBER ASUS_CSC_BUILD_NUMBER=$ASUS_CSC_BUILD_NUMBER ASUS_PROJECT_VERSION=$ASUS_PROJECT_VERSION otapackage -j$JOBS
+else
+	make BUILD_NUMBER=$BUILD_NUMBER ASUS_CSC_BUILD_NUMBER=$ASUS_CSC_BUILD_NUMBER ASUS_PROJECT_VERSION=$ASUS_PROJECT_VERSION -j$JOBS
+fi
+
 if [ $? -eq 0 ]; then
     echo "Build android ok!"
 else
@@ -88,22 +98,24 @@ fi
 
 # mkimage.sh
 echo "make and copy android images"
-./mkimage.sh
+if [ "$BUILD_OTA" = true ] ; then
+    INTERNAL_OTA_PACKAGE_OBJ_TARGET=obj/PACKAGING/target_files_intermediates/$TARGET_PRODUCT-target_files-$BUILD_NUMBER.zip
+    INTERNAL_OTA_PACKAGE_TARGET=$TARGET_PRODUCT-ota-$BUILD_NUMBER.zip
+    echo "generate ota package"
+    ./mkimage.sh ota
+    cp $OUT/$INTERNAL_OTA_PACKAGE_TARGET $IMAGE_PATH/
+    cp $OUT/$INTERNAL_OTA_PACKAGE_OBJ_TARGET $IMAGE_PATH/
+	sha256sum $IMAGE_PATH/$TARGET_PRODUCT-target_files-$BUILD_NUMBER.zip > $IMAGE_PATH/$TARGET_PRODUCT-target_files-$BUILD_NUMBER.zip.sha256sum
+	sha256sum $IMAGE_PATH/$TARGET_PRODUCT-ota-$BUILD_NUMBER.zip > $IMAGE_PATH/$TARGET_PRODUCT-ota-$BUILD_NUMBER.zip.sha256sum
+else
+	./mkimage.sh
+fi
+
 if [ $? -eq 0 ]; then
     echo "Make image ok!"
 else
     echo "Make image failed!"
     exit 1
-fi
-
-if [ "$BUILD_OTA" = true ] ; then
-    INTERNAL_OTA_PACKAGE_OBJ_TARGET=obj/PACKAGING/target_files_intermediates/$TARGET_PRODUCT-target_files-*.zip
-    INTERNAL_OTA_PACKAGE_TARGET=$TARGET_PRODUCT-ota-*.zip
-    echo "generate ota package"
-    make otapackage -j$JOBS
-    ./mkimage.sh ota
-    cp $OUT/$INTERNAL_OTA_PACKAGE_TARGET $IMAGE_PATH/
-    cp $OUT/$INTERNAL_OTA_PACKAGE_OBJ_TARGET $IMAGE_PATH/
 fi
 
 
@@ -126,11 +138,11 @@ rm $PACK_TOOL_DIR/rockdev/Image -rf
 mkdir -p $STUB_PATH
 
 #Generate patches
-echo "cwcw$PROJECT_TOP"
+echo "$PROJECT_TOP"
 .repo/repo/repo forall -c "$PROJECT_TOP/device/rockchip/common/gen_patches_body.sh"
 
 #Copy stubs
-cp manifest.xml $STUB_PATH/manifest_${DATE}.xml
+cp manifest.xml $STUB_PATH/manifest_$RELEASE_NAME.xml
 
 mkdir -p $STUB_PATCH_PATH/kernel
 cp kernel/.config $STUB_PATCH_PATH/kernel
@@ -142,3 +154,18 @@ cp $IMAGE_PATH/* $STUB_PATH/IMAGES/
 echo "UBOOT:  defconfig: $UBOOT_DEFCONFIG" >> $STUB_PATH/build_cmd_info
 echo "KERNEL: defconfig: $KERNEL_DEFCONFIG, dts: $KERNEL_DTS" >> $STUB_PATH/build_cmd_info
 echo "ANDROID:$DEVICE-$BUILD_VARIANT" >> $STUB_PATH/build_cmd_info
+
+# Delta package
+if [ $TARGET_FILES_OLD ]; then
+	echo "Start to build delta package..."
+	TARGET_FILES_NEW=$STUB_PATH/IMAGES/$TARGET_PRODUCT-target_files-$BUILD_NUMBER.zip
+	TARGET_FILES_FILENAME_NEW=$(basename $TARGET_FILES_NEW)
+	TARGET_FILES_FILENAME_OLD=$(basename $TARGET_FILES_OLD)
+	./build/tools/releasetools/ota_from_target_files -v -i $TARGET_FILES_OLD -p out/host/linux-x86 -k build/target/product/security/testkey $TARGET_FILES_NEW $STUB_PATH/IMAGES/Tinker_Board-AndroidN-Delta-${TARGET_FILES_FILENAME_NEW%.*}-from-${TARGET_FILES_FILENAME_OLD%.*}.zip
+	if [ $? -eq 0 ]; then
+	    echo "Succeed to generate the delta package."
+	else
+	    echo "Fail to generate the delta package."
+	    exit 1
+	fi
+fi
